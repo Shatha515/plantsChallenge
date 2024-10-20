1b8ad059fdd09a243e584029174b1fa313aa0bbb
Shatha515 <alshareefshatha34@gmail.com>
2024-10-20 10:17:14 +0300

    Initial Commit

---
+++ plantsChallenge/plantsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantsChallengeApp.swift
+//  plantsChallenge
+//
+//  Created by Shatha Alsharif on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plantsChallengeTests/plantsChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  plantsChallengeTests.swift
+//  plantsChallengeTests
+//
+//  Created by Shatha Alsharif on 17/04/1446 AH.
+//
+
+import Testing
+@testable import plantsChallenge
+
+struct plantsChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
